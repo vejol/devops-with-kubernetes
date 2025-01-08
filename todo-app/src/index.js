11e946f03ce49a1ad4d6fc535c423f7636dc9039
Vesa Jolkkonen <vesa.jolkkonen@hotmail.com>
2025-01-08 10:24:49 +0200

    Add exercise 1.05

--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -4,8 +4,18 @@ const PORT = process.env.PORT || 3001
 
 const server = createServer((req, res) => {
   res.statusCode = 200
-  res.setHeader("Content-Type", "text/plain")
-  res.end("Hello, World!\n")
+  res.setHeader("Content-Type", "text/html")
+  res.end(`<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Hello World</title>
+  </head>
+  <body>
+    <h1>Hello, World!</h1>
+  </body>
+</html>`)
 })
 
 server.listen(PORT)

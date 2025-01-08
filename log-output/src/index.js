37ca123068d7a698054bde16bd58a8fa0af7dfdc
Vesa Jolkkonen <vesa.jolkkonen@hotmail.com>
2025-01-08 15:29:46 +0200

    Add exercise 1.07

--- log-output/src/index.js
+++ log-output/src/index.js
@@ -1,7 +1,21 @@
 import { randomBytes } from "crypto"
+import { createServer } from "http"
 
-const randomString = randomBytes(10).toString("hex")
+const randomChars = randomBytes(10).toString("hex")
+let string
 
 setInterval(() => {
-  console.log(`${new Date().toISOString()}: ${randomString}`)
+  string = `${new Date().toISOString()}: ${randomChars}`
+  console.log(string)
 }, 5000)
+
+const PORT = process.env.PORT || 3001
+
+const server = createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader("Content-Type", "text/html")
+  res.end(string)
+})
+
+server.listen(PORT)
+console.log(`Server started in port ${PORT}`)

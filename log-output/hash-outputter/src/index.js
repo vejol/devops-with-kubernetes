dd412e647456bc2cb5be44a61bdb1d73af20c88f
Vesa Jolkkonen <vesa.jolkkonen@hotmail.com>
2025-01-09 21:21:21 +0200

    Add exercise 1.11

--- log-output/hash-outputter/src/index.js
+++ log-output/hash-outputter/src/index.js
@@ -3,20 +3,49 @@ import { createServer } from "http"
 import { readFileSync } from "fs"
 
 const PORT = process.env.PORT || 3001
-const filePath = path.join("/", "usr", "src", "app", "files", "hash.txt")
+
+const hashFilePath = path.join("/", "usr", "src", "app", "files", "hash.txt")
+const pingCountFilePath = path.join(
+  "/",
+  "usr",
+  "src",
+  "app",
+  "shared",
+  "pingcount.txt"
+)
 
 const readHash = () => {
   try {
-    return readFileSync(filePath, "utf8")
+    return readFileSync(hashFilePath, "utf8")
   } catch (err) {
     console.error("Error reading hash file", err)
+  }
+}
+
+const readPingCount = () => {
+  try {
+    return readFileSync(pingCountFilePath, "utf8")
+  } catch (err) {
+    console.error("Error reading ping count file", err)
+    return 0
   }
 }
 
 const server = createServer((req, res) => {
   res.statusCode = 200
   res.setHeader("Content-Type", "text/html")
-  res.end(readHash())
+  res.end(`<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>log-output</title>
+      </head>
+      <body>
+        <p>${readHash()}</p>
+        <p>Ping / Pongs: ${readPingCount()}</p>
+      </body>
+    </html>`)
 })
 
 server.listen(PORT)
--- ping-pong/src/index.js
+++ ping-pong/src/index.js
@@ -1,12 +1,30 @@
+import path from "path"
 import { createServer } from "http"
+import { writeFile, mkdirSync } from "fs"
 
 const PORT = process.env.PORT || 3001
 let counter = 0
+
+const directory = path.join("/", "usr", "src", "app", "shared")
+const filePath = path.join(directory, "pingcount.txt")
+
+mkdirSync(directory, { recursive: true }, (err) => {
+  if (err) {
+    console.error("Error creating directory", err)
+  }
+})
 
 const server = createServer((req, res) => {
   if (req.url === "/" || req.url === "/pingpong") {
     counter++
   }
+
+  writeFile(filePath, String(counter), (err) => {
+    if (err) {
+      console.error("Error writing to file", err)
+    }
+  })
+
   res.statusCode = 200
   res.setHeader("Content-Type", "text/html")
   res.end(`pong ${counter}`)

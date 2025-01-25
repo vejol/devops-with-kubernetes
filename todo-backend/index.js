6e40e27d63eacbc71065f545bd98fe702571e446
Vesa Jolkkonen <vesa.jolkkonen@hotmail.com>
2025-01-25 21:05:18 +0200

    Add exercise 2.08

--- todo-backend/index.js
+++ todo-backend/index.js
@@ -1,21 +1,62 @@
 const cors = require('cors')
 const express = require('express')
+const pg = require('pg')
 
 const PORT = process.env.PORT || 3002
-const todos = []
+const client = new pg.Client()
+
+client
+  .connect()
+  .then(() => {
+    console.log('Connected to PostgreSQL')
+    client.query(`
+      CREATE TABLE IF NOT EXISTS Todos (
+        id SERIAL PRIMARY KEY,
+        content VARCHAR(140) NOT NULL
+      )
+    `)
+  })
+  .catch((err) => {
+    console.error('Connection error', err.stack)
+    throw err
+  })
+
+getTodos = async () => {
+  try {
+    const result = await client.query('SELECT * FROM Todos')
+    return result.rows
+  } catch (err) {
+    console.error('Error fetching todos', err.stack)
+    throw err
+  }
+}
+
+addTodo = async (content) => {
+  try {
+    const result = await client.query(
+      'INSERT INTO Todos (content) VALUES ($1) RETURNING *',
+      [content]
+    )
+    return result.rows[0]
+  } catch (err) {
+    console.error('Error executing query', err.stack)
+    throw err
+  }
+}
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
-app.get('/todos', (req, res) => {
+app.get('/todos', async (req, res) => {
+  const todos = await getTodos()
   res.status(200).json(todos)
 })
 
-app.post('/todos', (req, res) => {
-  const todo = { id: Date.now(), content: req.body.content }
-  todos.push(todo)
+app.post('/todos', async (req, res) => {
+  const content = req.body.content
+  const todo = await addTodo(content)
   res.status(201).json(todo)
 })
 

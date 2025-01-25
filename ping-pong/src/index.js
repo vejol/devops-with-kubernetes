23b737123287a62d9411f5cd30cceb7b687389d5
Vesa Jolkkonen <vesa.jolkkonen@hotmail.com>
2025-01-25 15:49:55 +0200

    Add exercise 2.07

--- ping-pong/src/index.js
+++ ping-pong/src/index.js
@@ -1,19 +1,58 @@
 import { createServer } from 'http'
+import pg from 'pg'
 
 const PORT = process.env.PORT || 3001
-let counter = 0
+const client = new pg.Client()
 
-const server = createServer((req, res) => {
+client
+  .connect()
+  .then(() => {
+    console.log('Connected to PostgreSQL')
+    return client.query(`
+      CREATE TABLE IF NOT EXISTS pingpong (
+        id SERIAL PRIMARY KEY,
+        count INT NOT NULL
+      )
+    `)
+  })
+  .catch((err) => {
+    console.error('Connection error', err.stack)
+    throw err
+  })
+
+const getCounterValue = async () => {
+  try {
+    const result = await client.query('SELECT COUNT(*) FROM pingpong')
+    return parseInt(result.rows[0].count)
+  } catch (err) {
+    console.error('Error fetching counter value', err.stack)
+    throw err
+  }
+}
+
+const updateCounterValue = async (newCounterValue) => {
+  try {
+    await client.query('INSERT INTO pingpong (count) VALUES ($1)', [
+      newCounterValue,
+    ])
+  } catch (err) {
+    console.error('Error executing query', err.stack)
+    throw err
+  }
+}
+
+const server = createServer(async (req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/html')
 
   if (req.url === '/pingpong') {
-    counter++
-    res.end(`pong ${counter}`)
+    const newCounterValue = (await getCounterValue()) + 1
+    await updateCounterValue(newCounterValue)
+    res.end(`pong ${newCounterValue}`)
   }
 
   if (req.url === '/pingpong/count') {
-    res.end(`${counter}`)
+    res.end(`${await getCounterValue()}`)
   }
 })
 
